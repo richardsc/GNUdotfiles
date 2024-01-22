ca287096a0bf4192388da2d6311a70aa3e939f1d
Clark Richards <clark.richards@gmail.com>
2024-01-22 13:25:01 -0400

    change default emacs font size (not sure how this got changed ...)

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -48,5 +48,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "gray20" :foreground "white smoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "gray20" :foreground "white smoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
  '(hl-line ((t (:inherit highlight :background "grey28")))))
